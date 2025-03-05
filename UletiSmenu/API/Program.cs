7ba6622e03b637d2ba00c473c7d8a1b37f660c40
Luka Ladisic <l.ladisic@codenovasolutions.net>
2025-03-05 16:10:10 +0100

    Services registrated

---
+++ UletiSmenu/API/Program.cs
@@ -0,0 +1,132 @@
+using API.Middlewares;
+using Core.Models.Entities;
+using Core.Repositories;
+using Core.Services;
+using Infrastructure.Persistence.Database;
+using Infrastructure.Persistence.Database.Repositories;
+using Infrastructure.Persistence.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using Infrastructure.Email;
+using Core.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+builder.Services.AddTransient<ICompanyRepository, CompanyRepository>();
+
+builder.Services.AddScoped<UserManager<User>>();
+builder.Services.AddScoped<RoleManager<IdentityRole<Guid>>>();
+builder.Services.AddScoped<SignInManager<User>>();
+builder.Services.AddScoped<IApplicationUnitOfWork, ApplicationUnitOfWork>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ICompanyService, CompanyService>();
+
+builder.Services.Configure<SmtpSettings>(builder.Configuration.GetSection("SmtpSettings"))
+    .AddTransient(serviceProvider =>
+    {
+        var smtpSettings = serviceProvider.GetRequiredService<IOptions<SmtpSettings>>().Value;
+
+        return new SmtpClient
+        {
+            Host = smtpSettings.Host,
+            Port = smtpSettings.Port,
+            Credentials = new NetworkCredential(smtpSettings.Username, smtpSettings.Password),
+            EnableSsl = smtpSettings.EnableSsl
+        };
+    });
+
+builder.Services.AddTransient<IEmailService, EmailService>(provider =>
+{
+    var smtpClient = provider.GetRequiredService<SmtpClient>();
+    return new EmailService(smtpClient, (smtpClient.Credentials as NetworkCredential)!.UserName);
+});
+
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseSqlServer(
+        connectionString: builder.Configuration["ConnectionStrings:UletiSmenu"]
+    )
+);
+
+builder.Services.AddIdentity<User, IdentityRole<Guid>>(options =>
+{
+    options.SignIn.RequireConfirmedEmail = true;
+    options.User.RequireUniqueEmail = true;     
+    options.Password.RequireDigit = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 8;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            RequireExpirationTime = true,
+            ValidateLifetime = true
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddIdentityCore<User>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddApiEndpoints();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(policy =>
+    policy.AllowAnyOrigin()
+          .AllowAnyMethod()
+          .AllowAnyHeader());
+
+app.MapIdentityApi<User>();
+app.UseRouting();
+app.UseHttpsRedirection();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
