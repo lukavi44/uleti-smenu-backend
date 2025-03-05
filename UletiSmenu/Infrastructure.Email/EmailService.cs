1e6a061d4309425c9acd17b16f9d6d5d120f3d8a
Luka Ladisic <l.ladisic@codenovasolutions.net>
2025-03-05 16:09:52 +0100

    Email project done

---
+++ UletiSmenu/Infrastructure.Email/EmailService.cs
@@ -0,0 +1,26 @@
+﻿using Core.Interfaces;
+using System.Net.Mail;
+
+namespace Infrastructure.Email;
+
+public class EmailService : IEmailService
+{
+    private readonly SmtpClient _smtpClient;
+    private readonly string _fromEmail;
+
+    public EmailService(SmtpClient smtpClient, string fromEmail)
+    {
+        _smtpClient = smtpClient;
+        _fromEmail = fromEmail;
+    }
+
+    public async Task SendEmailAsync(string toEmail, string subject, string message)
+    {
+        var mailMessage = new MailMessage(_fromEmail, toEmail, subject, message);
+        mailMessage.IsBodyHtml = true;
+        _smtpClient.UseDefaultCredentials = false;
+        _smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+        await _smtpClient.SendMailAsync(mailMessage);
+    }
+}
