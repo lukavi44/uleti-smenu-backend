d1fd6a5d7d4d7129e27cb07d2c4361dde577623f
Luka Ladisic <l.ladisic@codenovasolutions.net>
2025-04-23 17:56:06 +0200

    Implement and register a custom UserClaimsPrincipalFactory

---
+++ UletiSmenu/API/RequestHelper/CustomClaimsPrincipalFactory.cs
@@ -0,0 +1,28 @@
+﻿using Core.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+
+namespace API.RequestHelper
+{
+    public class CustomClaimsPrincipalFactory : UserClaimsPrincipalFactory<User>
+    {
+        public CustomClaimsPrincipalFactory(
+            UserManager<User> userManager,
+            IOptions<IdentityOptions> optionsAccessor)
+            : base(userManager, optionsAccessor)
+        {
+        }
+
+        protected override async Task<ClaimsIdentity> GenerateClaimsAsync(User user)
+        {
+            var identity = await base.GenerateClaimsAsync(user);
+
+            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
+            identity.AddClaim(new Claim("userId", user.Id.ToString()));
+            identity.AddClaim(new Claim("email", user.Email ?? ""));
+
+            return identity;
+        }
+    }
+}
