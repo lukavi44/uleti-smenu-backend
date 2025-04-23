52431727f0551f70ab232ca921df1235b6b6b39c
Luka Ladisic <l.ladisic@codenovasolutions.net>
2025-04-23 17:55:20 +0200

    Mistake in data fix

--- UletiSmenu/API/DTOs/RegisterEmployeeDTO.cs
+++ UletiSmenu/API/DTOs/RegisterEmployeeDTO.cs
@@ -1,4 +1,4 @@
 ﻿namespace API.DTOs
 {
-    public record RegisterEmployeeDTO(string FirstName, string LastName, string Email, string UserName, string Password, string PhoneNumber, string ProfilePhoto, PIB PIB, MB MB);
+    public record RegisterEmployeeDTO(string FirstName, string LastName, string Email, string UserName, string Password, string PhoneNumber, string ProfilePhoto);
 }
