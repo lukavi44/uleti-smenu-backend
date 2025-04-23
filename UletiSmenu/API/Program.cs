be3353c0afc8d8a17bd0ad339219a840189828e0
Luka Ladisic <l.ladisic@codenovasolutions.net>
2025-04-23 17:57:22 +0200

    Remove cookies, use JWT; Register services and repositories

--- UletiSmenu/API/Program.cs
+++ UletiSmenu/API/Program.cs
@@ -1,27 +1,29 @@
 using API.Middlewares;
+using API.RequestHelper;
+using Core.Interfaces;
 using Core.Models.Entities;
 using Core.Repositories;
 using Core.Services;
+using Infrastructure.Email;
 using Infrastructure.Persistence.Database;
 using Infrastructure.Persistence.Database.Repositories;
 using Infrastructure.Persistence.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
-using System.Net.Mail;
 using System.Net;
-using System.Text;
-using Infrastructure.Email;
-using Core.Interfaces;
-using Microsoft.Extensions.FileProviders;
+using System.Net.Mail;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddTransient<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IJobPostRepository, JobPostRepository>();
+builder.Services.AddScoped<IUserClaimsPrincipalFactory<User>, CustomClaimsPrincipalFactory>();
+
 
 //builder.Services.AddScoped<UserManager<User>>();
 //builder.Services.AddScoped<SignInManager<User>>();
@@ -29,6 +31,7 @@ builder.Services.AddScoped<RoleManager<IdentityRole<Guid>>>();
 builder.Services.AddScoped<IApplicationUnitOfWork, ApplicationUnitOfWork>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IFileService, FileService>();
+builder.Services.AddScoped<IJobPostService, JobPostService>();
 
 builder.Services.Configure<SmtpSettings>(builder.Configuration.GetSection("SmtpSettings"))
     .AddTransient(serviceProvider =>
@@ -83,23 +86,16 @@ builder.Services.AddIdentity<User, IdentityRole<Guid>>(options =>
 .AddEntityFrameworkStores<ApplicationDbContext>()
 .AddDefaultTokenProviders();
 
-builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme)
-    .AddCookie(options =>
-    {
-        options.Events.OnRedirectToAccessDenied = context =>
-        {
-            context.Response.StatusCode = StatusCodes.Status403Forbidden;
-            return Task.CompletedTask;
-        };
-        options.Events.OnRedirectToLogin = context =>
-        {
-            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-            return Task.CompletedTask;
-        };
-    })
+builder.Services.AddAuthentication()
     .AddBearerToken(IdentityConstants.BearerScheme);
 
-builder.Services.AddAuthorization();
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = new AuthorizationPolicyBuilder()
+        .AddAuthenticationSchemes(IdentityConstants.BearerScheme)
+        .RequireAuthenticatedUser()
+        .Build();
+});
 
 builder.Services.AddIdentityCore<User>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
